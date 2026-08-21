6d1fcaf87dba44e42182e9f09ed9264b9581a598
khwggandol <khwggandol@gmail.com>
2026-08-21 19:32:26 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+// 오프라인 캐시. 네트워크 우선 + 실패 시 캐시 폴백이라,
+// 파일을 갈아끼워도 온라인이면 항상 최신이 뜬다.
+const CACHE = 'spectrum-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './acaan.html',
+  './spectrum.html',
+  './manifest.webmanifest',
+  './apple-touch-icon.png',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => Promise.allSettled(ASSETS.map(u => c.add(u))))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
